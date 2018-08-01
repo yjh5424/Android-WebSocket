c3a38819a507cb0f5a0943e8443289bcc87fcfce
Yun Jung Hyeon <yjh54240@naver.com>
2018-08-01 10:46:23 +0900

    init

---
+++ Android_WebSocket/app/src/main/java/com/study/mathpresso/android_websocket/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.study.mathpresso.android_websocket
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
